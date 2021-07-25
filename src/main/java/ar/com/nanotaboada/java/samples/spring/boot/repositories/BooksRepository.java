786b4d270ac9d7d073c41330177e84d2de294556
Nano Taboada <nanotaboada@users.noreply.github.com>
2021-07-25 17:06:37 -0300

    Added excludes for coverage report

--- src/main/java/ar/com/nanotaboada/java/samples/spring/boot/repositories/BooksRepository.java
+++ src/main/java/ar/com/nanotaboada/java/samples/spring/boot/repositories/BooksRepository.java
@@ -9,8 +9,11 @@ import java.util.Optional;
 
 @Repository
 public interface BooksRepository extends CrudRepository<Book, String> {
-
-    // Query creation from method names
-    // https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.query-creation
+    /**
+     * Query creation from method names <br />
+     * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.query-creation
+     * */
+    // Non-default methods in interfaces are not shown in coverage reports
+    // https://www.jacoco.org/jacoco/trunk/doc/faq.html
     Optional<Book> findByIsbn(String isbn);
 }
